544dd3befba06300572d2ca14e205f0af37ce354
liyijie <liyijie825@gmail.com>
2016-09-20 17:50:08 +0800

    update CloudClosetWechat Controller syntex error fix

--- app/controllers/cloud_closet_wechats_controller.rb
+++ app/controllers/cloud_closet_wechats_controller.rb
@@ -1,14 +1,14 @@
 class CloudClosetWechatsController < ApplicationController
-  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-controller-dsl
+  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-self-dsl
   wechat_responder appid: ENV["cloud_closet_app_id"], secret: ENV["cloud_closet_app_secret"], token: ENV["cloud_closet_app_token"], access_token: Rails.root.join(ENV["cloud_closet_app_access_token"])
 
   def js_hash
-    page_url = if controller.class.trusted_domain_fullname
-                 "#{controller.class.trusted_domain_fullname}#{controller.request.original_fullpath}"
+    page_url = if self.class.trusted_domain_fullname
+                 "#{self.class.trusted_domain_fullname}#{self.request.original_fullpath}"
                else
-                 controller.request.original_url
+                 self.request.original_url
                end
-    @js_hash = controller.wechat.jsapi_ticket.signature(page_url)
+    @js_hash = self.wechat.jsapi_ticket.signature(page_url)
   end
 
   on :text do |request, content|
