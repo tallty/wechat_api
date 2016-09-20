0dddbe9969dc8d19c3a2c5256dd18944b631529f
liyijie <liyijie825@gmail.com>
2016-09-20 18:01:53 +0800

    add cloud_closet_app_jsapi_ticket option

--- app/controllers/cloud_closet_wechats_controller.rb
+++ app/controllers/cloud_closet_wechats_controller.rb
@@ -1,6 +1,7 @@
 class CloudClosetWechatsController < ApplicationController
   # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-self-dsl
-  wechat_responder appid: ENV["cloud_closet_app_id"], secret: ENV["cloud_closet_app_secret"], token: ENV["cloud_closet_app_token"], access_token: Rails.root.join(ENV["cloud_closet_app_access_token"])
+  wechat_responder appid: ENV["cloud_closet_app_id"], secret: ENV["cloud_closet_app_secret"], token: ENV["cloud_closet_app_token"],
+      access_token: Rails.root.join(ENV["cloud_closet_app_access_token"]), jsapi_ticket: Rails.root.join(ENV["cloud_closet_app_jsapi_ticket"])
 
   def js_hash
     page_url = if self.class.trusted_domain_fullname
