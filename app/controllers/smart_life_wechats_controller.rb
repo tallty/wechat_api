02b88aa882e15fa2c677e10093d5cb309aa7e297
liyijie <liyijie825@gmail.com>
2017-08-09 16:38:53 +0800

    Add smart life wechat controller

---
+++ app/controllers/smart_life_wechats_controller.rb
@@ -0,0 +1,11 @@
+class SmartLifeWechatsController < ApplicationController
+  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-self-dsl
+  wechat_responder account: :smart_life
+
+  include ClientApiConcern
+
+  on :text do |request, content|
+    request.reply.text "echo: #{content}" # Just echo
+  end
+end
+
