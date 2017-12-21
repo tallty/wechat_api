e900b85ca9238d756261d0fa2ea7137ed606167d
liyijie <liyijie825@gmail.com>
2017-12-21 19:14:06 +0800

    Add innovation center API

---
+++ app/controllers/innovation_center_controller.rb
@@ -0,0 +1,10 @@
+class InnovationCenterController < ApplicationController
+  # For details on the DSL available within this file, see https://github.com/Eric-Guo/wechat#rails-responder-self-dsl
+  wechat_responder account: :innovation_center
+
+  include ClientApiConcern
+
+  on :text do |request, content|
+    request.reply.text "echo: #{content}" # Just echo
+  end
+end
